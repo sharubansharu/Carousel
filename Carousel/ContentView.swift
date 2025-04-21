e3a961d98ab075d68410de6625ac4e0e39e6a84d
sharuban <s.s.ruban98@gmail.com>
2025-04-21 19:58:39 +0530

    Initial Commit

---
+++ Carousel/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Carousel
+//
+//  Created by Sharuban Sharu on 4/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let images = [ "1","2","3","4","5"]
+    var body: some View {
+        ScrollView(.horizontal){
+            LazyHStack{
+                ForEach(images, id: \.self){
+                    image in Image(image)
+                        .resizable()
+                        .containerRelativeFrame(.horizontal)
+                        .clipShape(RoundedRectangle(cornerRadius:20))
+                        .scrollTransition { content, phase in content
+                                .scaleEffect(
+                                    x : phase.isIdentity ? 1.0 : 0.8,
+                                    y : phase.isIdentity ? 1.0 : 0.8
+                                )
+                                .offset(y :phase.isIdentity ? 0: 200)
+                        }
+                }
+            }
+        }
+        .contentMargins(20)
+        .background(.indigo)
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CarouselTests/CarouselTests.swift
@@ -0,0 +1,16 @@
+//
+//  CarouselTests.swift
+//  CarouselTests
+//
+//  Created by Sharuban Sharu on 4/21/25.
+//
+
+import Testing
+
+struct CarouselTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
